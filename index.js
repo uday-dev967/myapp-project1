e317ea1e7ea6517568aa8835cf5d11f418d24b7a
udaykanda967 <udaychakravarthi22@gmail.com>
2022-11-02 07:00:59 +0000

    adds Get Books API and related files

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+//console.log(__dirname);
+
+const dbpath = path.join(__dirname, "goodreads.db");
+let db = null;
+
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `SELECT 
+    *
+    FROM 
+        book
+    ORDER BY
+        book_id`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbpath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server running at https://localhost:3000/");
+    });
+  } catch (e) {
+    console.log(`DB Error : ${e.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
